7208733e29285df18d3696cda5978e6ba606da69
chanasia <asiavip2546@gmail.com>
2023-12-26 02:08:27 +0700

    Update script.

--- scraping_comments_from_group_fb/main.py
+++ scraping_comments_from_group_fb/main.py
@@ -117,74 +117,61 @@ with sqlite3.connect("posts.db") as connection:
     )
 
 
-def replies_scraping(box_comment):
-    try:
-        replies_url = box_comment.find_element(
-            By.CSS_SELECTOR, "div > div:nth-of-type(4) > div[id] > div a[href]"
-        ).get_attribute("href")
-        
-        driver.execute_script(f"window.open('{replies_url}', '_blank')")
-        time.sleep(1)
-        driver.switch_to.window(driver.window_handles[2])
-    except:
-        return []
-    
-    you_is_block = False
+def replies_scraping():
     try:
         you_is_block = driver.find_element(
-            By.CSS_SELECTOR,
-            'div[title="You’re Temporarily Blocked"] > h2',
+            By.XPATH,
+            '//div[@title="You’re Temporarily Blocked"]/h2',
         ).text
-        
+
         print("You’re Temporarily Blocked from viewing replies.")
-        you_is_block = True
+        sys.exit(1)
     except:
         None
-    if you_is_block: sys.exit(1)
-
-    reply_id = re.search(r"ctoken=(\d+_\d+)", driver.current_url).group(1)
+    
     replies = []
+    next_page_btn_id = None
 
     while True:
         box_replies = driver.find_elements(
-            By.CSS_SELECTOR,
-            'div[id="root"] > div[class] > div[id] + div > div:has(div)',
+            By.XPATH,
+            '//div[@id="root"]/div[@class]/div[not(@id)]/div[div]',
         )
-        for idx, box in enumerate(box_replies):
-            reply = dict()
-            reply["reply_by"] = box.find_element(By.CSS_SELECTOR, "div > h3").text
+        if len(box_replies) > 0:
+            for idx, box in enumerate(box_replies):
+                reply = dict()
+                reply["reply_by"] = box.find_element(By.XPATH, 'div/h3').text
+                try:
+                    reply["reply_to"] = box.find_element(By.XPATH, 'div/div[1]/a').text
+                except:
+                    None
+                reply_array = box.find_elements(By.XPATH, "div/div[1]")
+                reply_comment = ''.join([span.text for span in reply_array])
+                if not reply.get("reply_to", None) is None: reply_comment = reply_comment.replace(f'{reply.get("reply_to", None)} ','')
+                reply["reply"] = reply_comment
+                reply["reply_order"] = idx
+                
+                print(f"{reply['reply_by']}{ ' To ' + reply.get('reply_to', None) if reply.get('reply_to', None) else None } -> reply: {reply['reply']}")
+                replies.append(reply)
+                
+        if next_page_btn_id is None:
             try:
-                reply["reply_to"] = box.find_element(
-                    By.CSS_SELECTOR, "div > h3 + div > a"
-                ).text
+                next_page_btn_id = driver.find_element(By.XPATH, '//div[@id="root"]//div[starts-with(@id, "comment_replies_more_")]').get_attribute('id')
             except:
-                None
-            reply_array = box.find_elements(
-                By.CSS_SELECTOR, "div > h3 + div"
-            )
-            reply_comment = (''.join([reply_span.text for reply_span in reply_array]))
-            if reply.get("reply_to", None): reply_comment = reply_comment.replace(reply.get("reply_to", None),'')
-            reply['reply'] = reply_comment
-            reply["reply_order"] = idx
-            replies.append(reply)
-            print(f"{reply['reply_by']}{ ' To ' + reply.get('reply_to', None) if reply.get('reply_to', None) else None } -> reply: {reply['reply']}")
+                break
         try:
-            prev_comment_btn = WebDriverWait(driver, 2).until(
-                EC.element_to_be_clickable(
+            next_page_btn = WebDriverWait(driver, 2).until(
+                EC.element_to_be_clicskable(
                     (
-                        By.CSS_SELECTOR,
-                        f'div[id="root"] > div[class] > div[id] + div > div[id="comment_replies_more_1:{reply_id}"] > a',
+                        By.XPATH,
+                        f'//div[@id="root"]//div[@id="{next_page_btn_id}"]/a',
                     )
                 )
             )
-            prev_comment_btn.click()
+            next_page_btn.click()
         except:
             break
-    driver.close()
-    time.sleep(1)
-    driver.switch_to.window(driver.window_handles[1])
     return replies
-
 
 def comments_scraping():
     post_id = re.search(r"/permalink/(\d+)", driver.current_url).group(1)
@@ -193,51 +180,52 @@ def comments_scraping():
 
     while True:
         box_comments = driver.find_elements(
-            By.CSS_SELECTOR,
-            "#m_story_permalink_view > div[id] > div > div:not([id]) > div:has(div)",
+            By.XPATH,
+            '//*[@id="m_story_permalink_view"]/div[@id]/div/div[not(@id)]/div[div]',
         )
-        if len(box_comments) == 0:
-            return []
-        for box_comment in box_comments:
-            comment = dict()
-            comment["comment_by"] = box_comment.find_element(
-                By.CSS_SELECTOR, "div > h3"
-            ).text
-            comment["comment"] = box_comment.find_element(
-                By.CSS_SELECTOR, "div > div:nth-child(2)"
-            ).text
-            print(f"{comment['comment_by']} -> comment: {comment['comment']}")
-            driver.switch_to.window(driver.window_handles[1])
+        if len(box_comments) > 0:
+            for box_comment in box_comments:
+                comment = dict()
+                comment["comment_by"] = box_comment.find_element(By.XPATH, "div/h3").text
+                comment["comment"] = box_comment.find_element(By.XPATH, "div/div[1]").text
+                print(f"{comment['comment_by']} -> comment: {comment['comment']}")
 
-            replies = replies_scraping(box_comment)
-            if len(replies) != 0:
-                comment["replies"] = replies
-
-            comments.append(comment)
+                replies_href = None
+                try:
+                    replies_href = box_comment.find_element(
+                        By.XPATH, 
+                        'div[last()]/div/div//a[contains(text(), "replied")]').get_attribute('href')
+                except:
+                    None
+                if not replies_href is None:
+                    driver.execute_script(f"window.open('{replies_href}', '_blank')")
+                    time.sleep(1)
+                    driver.switch_to.window(driver.window_handles[2])
+                    replies = replies_scraping()
+                    if len(replies) != 0:
+                        comment["replies"] = replies
+                    driver.close()
+                    time.sleep(1)
+                    driver.switch_to.window(driver.window_handles[1])    
+                comments.append(comment)
 
         if next_page_btn_id is None:
-            try: 
+            try:
                 next_page_btn_id = driver.find_element(
-                    By.CSS_SELECTOR,
-                    f'#m_story_permalink_view div:last-of-type > div > div:nth-of-type(5) > div:has(>a)',
+                    By.XPATH,
+                    '//*[@id="m_story_permalink_view"]/div[last()]/div/div[not(@id)]/div[a]'
                 ).get_attribute('id')
             except:
                 break
         try:
             next_page_btn = WebDriverWait(driver, 2).until(
-                EC.element_to_be_clickable(
-                    (
-                        By.CSS_SELECTOR,
-                        f'div[id="root"] div#{next_page_btn_id} > a',
-                    )
-                )
+                EC.element_to_be_clickable((By.XPATH, f'//div[@id="root"]//div[@id="{next_page_btn_id}"]/a'))
             )
             next_page_btn.click()
         except:
             break
     print(f"Complete post_id: {post_id}")
     return comments
-
 
 def post_scraping():
     post_id = re.search(r"/permalink/(\d+)", driver.current_url).group(1)
@@ -267,67 +255,65 @@ def reset_tab():
 
 
 while True:
-    anchor_all = driver.find_elements( #The program requires a discussion thread.
-        By.CSS_SELECTOR,
-        "#m_group_stories_container article > footer > div:last-child > a:nth-of-type(3)",
+    anchor_all = driver.find_elements(
+        By.XPATH, '//article/footer/div[last()]/a[contains(text(), "Full Story")]'
     )
     anchor_all = [a.get_attribute("href") for a in anchor_all]
     anchor_post_shares = driver.find_elements(
-        By.CSS_SELECTOR,
-        "article:has(article) > footer > div:last-child > a:nth-of-type(3)",
+        By.XPATH,
+        '//article[descendant::article]/footer/div[last()]/a[contains(text(), "Full Story")]',
     )
     anchor_post_shares = [a.get_attribute("href") for a in anchor_post_shares]
     anchors = list(set(anchor_all) - set(anchor_post_shares))
-    print(anchors)
-    for a in anchors:
-        post_id = re.search(r"/permalink/(\d+)", a).group(1)
-        with sqlite3.connect("posts.db") as connection:
-            cursor = connection.cursor()
-            rows = cursor.execute(
-                "SELECT post_id FROM posts WHERE post_id=?", (post_id,)
-            ).fetchall()
-            if len(rows) != 0:
-                print(f"Already have post_id : {post_id}")
-                continue
-        time.sleep(1)
-        driver.execute_script(f"window.open('{a}', '_blank')")
-        time.sleep(1)
-        driver.switch_to.window(driver.window_handles[1])
-        print(f"Start scraping post_id: {post_id}.")
-        print(a)
-        post = post_scraping()
-
-        with sqlite3.connect("posts.db") as connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                "INSERT INTO posts (post_id, topic_by, topic) VALUES (?, ?, ?)",
-                (post["post_id"], post["topic_by"], post["topic"]),
-            )
-            post_id1 = cursor.lastrowid
-            comments = post.get("comments", [])
-            if len(comments) == 0:
-                continue
-            for comment in comments:
-                cursor.execute(
-                    "INSERT INTO comments (comment_by, comment, post_id) VALUES (?, ?, ?)",
-                    (comment["comment_by"], comment["comment"], post_id1),
-                )
-                comment_id = cursor.lastrowid
-                replies = comment.get("replies", [])
-                if len(replies) == 0:
+    if len(anchors) > 0:
+        for a in anchors:
+            post_id = re.search(r"/permalink/(\d+)", a).group(1)
+            with sqlite3.connect("posts.db") as connection:
+                cursor = connection.cursor()
+                rows = cursor.execute(
+                    "SELECT post_id FROM posts WHERE post_id=?", (post_id,)
+                ).fetchall()
+                if len(rows) != 0:
+                    print(f"Already have post_id : {post_id}")
                     continue
-                for reply in replies:
-                    cursor.execute(
-                        "INSERT INTO replies (reply_by, reply_to, reply, reply_order, comment_id) VALUES (?, ?, ?, ?, ?)",
-                        (reply["reply_by"], reply.get("reply_to", None), reply["reply"], reply['reply_order'], comment_id),
-                    )
-            connection.commit()
-        print(f"Save to database complated as post_id: {post_id}.")
+            driver.execute_script(f"window.open('{a}', '_blank')")
+            time.sleep(1)
+            driver.switch_to.window(driver.window_handles[1])
+            print(f"Start scraping post_id: {post_id}.")
 
-        driver.close()
-        time.sleep(1)
-        driver.switch_to.window(driver.window_handles[0])
-        time.sleep(1)
+            post = post_scraping()
+
+            with sqlite3.connect("posts.db") as connection:
+                cursor = connection.cursor()
+                cursor.execute(
+                    "INSERT INTO posts (post_id, topic_by, topic) VALUES (?, ?, ?)",
+                    (post["post_id"], post["topic_by"], post["topic"]),
+                )
+                post_id1 = cursor.lastrowid
+                comments = post.get("comments", [])
+                if len(comments) == 0:
+                    continue
+                for comment in comments:
+                    cursor.execute(
+                        "INSERT INTO comments (comment_by, comment, post_id) VALUES (?, ?, ?)",
+                        (comment["comment_by"], comment["comment"], post_id1),
+                    )
+                    comment_id = cursor.lastrowid
+                    replies = comment.get("replies", [])
+                    if len(replies) == 0:
+                        continue
+                    for reply in replies:
+                        cursor.execute(
+                            "INSERT INTO replies (reply_by, reply_to, reply, reply_order, comment_id) VALUES (?, ?, ?, ?, ?)",
+                            (reply["reply_by"], reply.get("reply_to", None), reply["reply"], reply['reply_order'], comment_id),
+                        )
+                connection.commit()
+            print(f"Save to database complated as post_id: {post_id}.")
+
+            driver.close()
+            time.sleep(1)
+            driver.switch_to.window(driver.window_handles[0])
+            time.sleep(1)
     try:
         see_more_posts_btn = WebDriverWait(driver, 2).until(
             EC.element_to_be_clickable(
